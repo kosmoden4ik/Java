38086e93a3b104c40f65a9a26eb793d6bd315cfe
kosmoden4ik <kosmoden4ik@gmail.com>
2022-03-27 22:07:24 +0300

    MASTER 9.2.

---
+++ src/homework_9_2/Main.java
@@ -0,0 +1,36 @@
+package homework_9_2;
+/*
+ Создайте проект, используя IntelliJ IDEA. Создайте класс MyList.
+ Реализуйте в простейшем приближении возможность использования
+ его экземпляра аналогично экземпляру класса List.
+ Минимально требуемый интерфейс взаимодействия с экземпляром,
+ должен включать метод добавления элемента,
+ индексатор для получения значения элемента по указанному индексу
+ и свойство только для чтения для получения общего количества элементов.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyList <Integer> malmo=new MyList();
+        malmo.addValue(12);
+        malmo.addValue(22);
+        malmo.addValue(33);
+        malmo.addValue(44);
+        malmo.addValue(55);
+        malmo.addValue(66);
+        malmo.addValue(77);
+        malmo.addValue(88);
+        malmo.addValue(99);
+        malmo.addValue(0);
+        malmo.addValue(1);
+        malmo.addValue(2);
+        malmo.addValue(3);
+        malmo.addValue(4);
+        System.out.println(malmo.getValue(0));
+        System.out.println(malmo.getValue(10));
+        System.out.println("Длина списка - "+malmo.length());
+
+        MyList <String> arrayVar=new MyList();
+        arrayVar.addValue("OlonDjonson");
+        System.out.println(arrayVar.getValue(0));
+    }
+}
---
+++ src/homework_9_2/MyList.java
@@ -0,0 +1,44 @@
+package homework_9_2;
+/*
+ Создайте проект, используя IntelliJ IDEA. Создайте класс MyList.
+ Реализуйте в простейшем приближении возможность использования
+ его экземпляра аналогично экземпляру класса List.
+ Минимально требуемый интерфейс взаимодействия с экземпляром,
+ должен включать метод добавления элемента,
+ индексатор для получения значения элемента по указанному индексу
+ и свойство только для чтения для получения общего количества элементов.
+ */
+public class MyList<T> {
+    T valueList[]=(T[])new Object[10];
+    int indexSize=0;
+   MyList(){
+       // varList=new T [10];
+    }
+    void addValue(T valueList){
+        if(indexSize==this.valueList.length-1){
+            resizeList();
+        }
+       this.valueList[indexSize]=valueList;
+       indexSize++;
+    }
+    private void resizeList(){
+       T newList[]=(T[])new Object[valueList.length+10];
+       for(int i = 0; i< valueList.length; i++){
+           newList[i]= (T) valueList[i];
+       }
+
+        valueList =newList;
+    }
+    public T getValue(int index){
+       if(index< valueList.length) {
+return valueList[index];
+       }
+       else{
+           throw new RuntimeException("Выход за диапазон");
+       }
+    }
+
+    int length(){
+       return indexSize;
+    }
+}
